cdfeeb97dfb2417fdf66dc446d5b89310e5af431
axf_1_0 <244178627+QF-A@users.noreply.github.com>
2026-05-12 15:24:30 +0800

    feat: 更新music栏目

--- src/config/musicConfig.ts
+++ src/config/musicConfig.ts
@@ -11,7 +11,7 @@ export const musicPlayerConfig: MusicPlayerConfig = {
 	showInNavbar: true,
 
 	// 使用方式："meting" 使用 Meting API，"local" 使用本地音乐列表
-	mode: "meting",
+	mode: "local",
 
 	// 默认音量 (0-1)
 	volume: 0.7,
@@ -28,7 +28,7 @@ export const musicPlayerConfig: MusicPlayerConfig = {
 		// 默认使用官方 API，也可以使用自定义 API
 		api: "https://api.i-meto.com/meting/api?server=:server&type=:type&id=:id&r=:r",
 		// 音乐平台：netease=网易云音乐, tencent=QQ音乐, kugou=酷狗音乐, xiami=虾米音乐, baidu=百度音乐
-		server: "netease",
+		server: "kugou",
 		// 类型：song=单曲, playlist=歌单, album=专辑, search=搜索, artist=艺术家
 		type: "playlist",
 		// 歌单/专辑/单曲 ID 或搜索关键词
@@ -49,11 +49,53 @@ export const musicPlayerConfig: MusicPlayerConfig = {
 	// lrc: "[00:00.00]歌词内容...",
 	local: {
 		playlist: [
+			// {
+			// 	name: "使一颗心免于哀伤",
+			// 	artist: "知更鸟 / HOYO-MiX / Chevy",
+			// 	url: "/assets/music/使一颗心免于哀伤-哼唱.mp3",
+			// 	cover: "/assets/music/cover/109951169585655912.webp",
+			// 	lrc: "",
+			// },
 			{
-				name: "使一颗心免于哀伤",
-				artist: "知更鸟 / HOYO-MiX / Chevy",
-				url: "/assets/music/使一颗心免于哀伤-哼唱.mp3",
-				cover: "/assets/music/cover/109951169585655912.webp",
+				name: "演员",
+				artist: "薛之谦",
+				url: "/assets/music/mp3/薛之谦 - 演员.mp3",
+				cover: "/assets/music/cover/d69ccadcde72553dad22b8e2883dfcce.jpg",
+				lrc: "",
+			},
+			{
+				name: "绅士",
+				artist: "薛之谦",
+				url: "/assets/music/mp3/薛之谦 - 绅士_kgg-dec.mp3",
+				cover: "/assets/music/cover/d69ccadcde72553dad22b8e2883dfcce.jpg",
+				lrc: "",
+			},
+			{
+				name: "刚刚好",
+				artist: "薛之谦",
+				url: "/assets/music/mp3/薛之谦 - 刚刚好.mp3",
+				cover: "/assets/music/cover/0d48f96f7337430e13de0fb4ef977548.jpg",
+				lrc: "",
+			},
+			{
+				name: "我好像在哪见过你",
+				artist: "薛之谦",
+				url: "/assets/music/mp3/薛之谦 - 我好像在哪见过你_kgg-dec.mp3",
+				cover: "/assets/music/cover/0d48f96f7337430e13de0fb4ef977548.jpg",
+				lrc: "",
+			},
+			{
+				name: "江南",
+				artist: "林俊杰",
+				url: "/assets/music/mp3/林俊杰 - 江南_kgg-dec.mp3",
+				cover: "/assets/music/cover/fbf5dc141e3107361f61269ccf2a6f0b.jpg",
+				lrc: "",
+			},
+			{
+				name: "Always Online",
+				artist: "林俊杰",
+				url: "/assets/music/mp3/林俊杰 - Always Online_kgg-dec.mp3",
+				cover: "/assets/music/cover/254395920e8a8dd4f79ab21476be7a00.jpg",
 				lrc: "",
 			},
 		],
